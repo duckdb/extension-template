d8792b9bef88c1e1deef9c3c38c876b9a0a9b19b
Sam Ansmink <samansmink@hotmail.com>
2023-02-13 17:19:22 +0100

    deploy to ../latest/..

--- scripts/extension-upload.sh
+++ scripts/extension-upload.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Usage: ./extension-upload.sh <name> <Extension commithash or version_tag> <DuckDB commithash or version_tag> <architecture> <s3_bucket>
+# Usage: ./extension-upload.sh <name> <extension_version> <duckdb_version> <architecture> <s3_bucket> <copy_to_latest>
+# <name>                : Name of the extension
+# <extension_version>   : Version (commit / version tag) of the extension
+# <duckdb_version>      : Version (commit / version tag) of DuckDB
+# <architecture>        : Architecture target of the extension binary
+# <s3_bucket>           : S3 bucket to upload to
+# <copy_to_latest>      : Set this as the latest version ("true" / "false", default: "false")
 
 set -e
 
@@ -11,3 +17,9 @@ gzip < $ext > "$1.duckdb_extension.gz"
 
 # upload compressed extension binary to S3
 aws s3 cp $1.duckdb_extension.gz s3://$5/$1/$2/$3/$4/$1.duckdb_extension.gz --acl public-read
+
+if [ $6 = 'true']
+then
+  aws s3 cp $1.duckdb_extension.gz s3://$5/$1/latest/$3/$4/$1.duckdb_extension.gz --acl public-read
+fi
+# also uplo
