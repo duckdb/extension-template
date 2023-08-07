bd0df0a91e2f46e34963bcda8ba91c7fac966dd9
Sam Ansmink <samansmink@hotmail.com>
2023-08-07 15:44:09 +0200

    try out gh pages deployment

--- scripts/extension-upload.sh
+++ scripts/extension-upload.sh
@@ -5,20 +5,20 @@
 # <extension_version>   : Version (commit / version tag) of the extension
 # <duckdb_version>      : Version (commit / version tag) of DuckDB
 # <architecture>        : Architecture target of the extension binary
-# <s3_bucket>           : S3 bucket to upload to
+# <output_dir>          : Output directory to write files to
 # <copy_to_latest>      : Set this as the latest version ("true" / "false", default: "false")
 
 set -e
 
-ext="build/release/extension/$1/$1.duckdb_extension"
+ext="/tmp/extension_download/$1.duckdb_extension"
 
 # compress extension binary
 gzip < "${ext}" > "$ext.gz"
 
 # upload compressed extension binary to S3
-aws s3 cp $ext.gz s3://$5/$1/$2/$3/$4/$1.duckdb_extension.gz --acl public-read
+cp $ext.gz $5/$1/$2/$3/$4/$1.duckdb_extension.gz
 
 # upload to latest if copy_to_latest is set to true
 if [[ $6 = 'true' ]]; then
-  aws s3 cp $ext.gz s3://$5/$1/latest/$3/$4/$1.duckdb_extension.gz --acl public-read
+  cp $ext.gz $5/$1/latest/$3/$4/$1.duckdb_extension.gz
 fi
