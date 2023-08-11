55d39462ec1422f2444b2d3d69fc1251f9569883
Sam Ansmink <samansmink@hotmail.com>
2023-08-11 16:23:15 +0200

    revert change to extension-upload script

--- scripts/extension-upload.sh
+++ scripts/extension-upload.sh
@@ -5,20 +5,20 @@
 # <extension_version>   : Version (commit / version tag) of the extension
 # <duckdb_version>      : Version (commit / version tag) of DuckDB
 # <architecture>        : Architecture target of the extension binary
-# <output_dir>          : Output directory to write files to
+# <s3_bucket>           : S3 bucket to upload to
 # <copy_to_latest>      : Set this as the latest version ("true" / "false", default: "false")
 
 set -e
 
-ext="/tmp/extension_download/$1.duckdb_extension"
+ext="build/release/extension/$1/$1.duckdb_extension"
 
 # compress extension binary
 gzip < "${ext}" > "$ext.gz"
 
 # upload compressed extension binary to S3
-cp $ext.gz $5/$1/$2/$3/$4/$1.duckdb_extension.gz
+aws s3 cp $ext.gz s3://$5/$1/$2/$3/$4/$1.duckdb_extension.gz --acl public-read
 
 # upload to latest if copy_to_latest is set to true
 if [[ $6 = 'true' ]]; then
-  cp $ext.gz $5/$1/latest/$3/$4/$1.duckdb_extension.gz
+  aws s3 cp $ext.gz s3://$5/$1/latest/$3/$4/$1.duckdb_extension.gz --acl public-read
 fi
