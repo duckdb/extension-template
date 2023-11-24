b804f8ee2bad315a324e9a91502cf6c4b367950d
Carlo Piovesan <piovesan.carlo@gmail.com>
2023-11-24 12:35:34 +0100

    Properly put extensions in the duckdb-wasm subfolder

--- scripts/extension-upload-wasm.sh
+++ scripts/extension-upload-wasm.sh
@@ -57,5 +57,5 @@ fi
 
 # upload to latest version
 if [[ $6 = 'true' ]]; then
-  aws s3 cp $ext.gz s3://$5/$3/$4/$1.duckdb_extension.wasm --acl public-read --content-encoding br --content-type="application/wasm"
+  aws s3 cp $ext.gz s3://$5/duckdb-wasm/$3/$4/$1.duckdb_extension.wasm --acl public-read --content-encoding br --content-type="application/wasm"
 fi
