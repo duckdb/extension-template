075d8557f14f0d0d0a6420ed881cbaec15489918
Carlo Piovesan <piovesan.carlo@gmail.com>
2023-11-28 15:19:19 +0100

    Unify scripts

--- scripts/extension-upload.sh
+++ scripts/extension-upload.sh
@@ -13,12 +13,32 @@
 
 set -e
 
-ext="/tmp/extension/$1.duckdb_extension"
+if [[ $ext == "wasm_*" ]]; then
+  ext="/tmp/extension/$1.duckdb_extension.wasm"
+else
+  ext="/tmp/extension/$1.duckdb_extension"
+fi
 
 script_dir="$(dirname "$(readlink -f "$0")")"
 
 # calculate SHA256 hash of extension binary
 cat $ext > $ext.append
+
+if [[ $4 == "wasm_*" ]]; then
+  # 0 for custom section
+  # 113 in hex = 275 in decimal, total lenght of what follows (1 + 16 + 2 + 256)
+  # [1(continuation) + 0010011(payload) = \x93, 0(continuation) + 10(payload) = \x02]
+  echo -n -e '\x00' >> $ext.append
+  echo -n -e '\x93\x02' >> $ext.append
+  # 10 in hex = 16 in decimal, lenght of name, 1 byte
+  echo -n -e '\x10' >> $ext.append
+  echo -n -e 'duckdb_signature' >> $ext.append
+  # the name of the WebAssembly custom section, 16 bytes
+  # 100 in hex, 256 in decimal
+  # [1(continuation) + 0000000(payload) = ff, 0(continuation) + 10(payload)],
+  # for a grand total of 2 bytes
+  echo -n -e '\x80\x02' >> $ext.append
+fi
 
 # (Optionally) Sign binary
 if [ "$DUCKDB_EXTENSION_SIGNING_PK" != "" ]; then
@@ -35,7 +55,12 @@ truncate -s 256 $ext.sign
 cat $ext.sign >> $ext.append
 
 # compress extension binary
-gzip < $ext.append > "$ext.gz"
+if [[ $4 == "wasm_*" ]]; then
+  gzip < $ext.append > "$ext.compressed"
+else
+  brotli < $ext.append > "$ext.compressed"
+fi
+
 set -e
 
 # Abort if AWS key is not set
@@ -46,10 +71,18 @@ fi
 
 # upload versioned version
 if [[ $7 = 'true' ]]; then
-  aws s3 cp $ext.gz s3://$5/$1/$2/$3/$4/$1.duckdb_extension.gz --acl public-read
+  if [[ $4 == "wasm_*" ]]; then
+    aws s3 cp $ext.compressed s3://$5/duckdb-wasm/$1/$2/duckdb-wasm/$3/$4/$1.duckdb_extension.wasm --acl public-read --content-encoding br --content-type="application/wasm"
+  else
+    aws s3 cp $ext.compressed s3://$5/$1/$2/$3/$4/$1.duckdb_extension.gz --acl public-read
+  fi
 fi
 
 # upload to latest version
 if [[ $6 = 'true' ]]; then
-  aws s3 cp $ext.gz s3://$5/$3/$4/$1.duckdb_extension.gz --acl public-read
+  if [[ $4 == "wasm_*" ]]; then
+    aws s3 cp $ext.compressed s3://$5/duckdb-wasm/$3/$4/$1.duckdb_extension.wasm --acl public-read --content-encoding br --content-type="application/wasm"
+  else
+    aws s3 cp $ext.compressed s3://$5/$3/$4/$1.duckdb_extension.gz --acl public-read
+  fi
 fi
