30e1914fce07a7619c886c78947cb2d19fb3840b
Sam Ansmink <samansmink@hotmail.com>
2024-02-28 13:28:58 +0100

    move makefile to extension-ci-tools

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 PROJ_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 
+# Configuration of extension
 EXT_NAME=quack
 EXT_CONFIG=${PROJ_DIR}extension_config.cmake
 
-include out-of-tree-extension.Makefile
+# Include the Makefile from extension-ci-tools
+include extension-ci-tools/makefiles/duckdb_extension.Makefile
--- extension_config.cmake
+++ extension_config.cmake
@@ -1,11 +1,10 @@
-# Config to build this extension
+# This file is included by DuckDB's build system. It specifies which extension to load
+
+# Extension from this repo
 duckdb_extension_load(quack
     SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
-    INCLUDE_PATH ${CMAKE_CURRENT_LIST_DIR}src/include
-    TEST_PATH ${CMAKE_CURRENT_LIST_DIR}src/include
     LOAD_TESTS
-    DONT_LINK
 )
 
-# Load more extensions in your builds by adding them here. E.g.:
-# duckdb_extension_load(json)
+# Any extra extensions
+# e.g.: duckdb_extension_load(json)
