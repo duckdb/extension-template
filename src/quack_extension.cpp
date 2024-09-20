f21173a1fcd9435039f9f10edf84e3cd49dddc69
Alex-Monahan <alex@motherduck.com>
2024-09-20 07:13:58 -0700

    Trim SQL Macro examples (remove comments and some tests, move to bottom)

--- src/quack_extension.cpp
+++ src/quack_extension.cpp
@@ -15,54 +15,6 @@
 
 namespace duckdb {
 
-
-// To add a new scalar SQL macro, add a new macro to this array!
-// Copy and paste the top item in the array into the 
-// second-to-last position and make some modifications. 
-// (essentially, leave the last entry in the array as {nullptr, nullptr, {nullptr}, {{nullptr, nullptr}}, nullptr} )
-
-// Keep the DEFAULT_SCHEMA (no change needed)
-// Replace "times_two" with a name for your macro
-// If your function has parameters without default values, add their names in quotes inside of the {}, with a nullptr at the end
-//      If you do not have parameters, simplify to {nullptr}
-// If your function has parameters with default values, add their names and values in double quotes inside of {}'s inside of the {}.
-//      If you have default values that are strings, wrap them in single quotes inside the double quotes 
-//          (Ex: {{"my_string":"'my_default_value'"}, {nullptr, nullptr}} )
-//      Be sure to keep {nullptr, nullptr} at the end
-//      If you do not have parameters with default values, simplify to {nullptr, nullptr}
-// Add the text of your SQL macro as a raw string with the format R"( select 42 )"
-// clang-format off
-static DefaultMacro quack_macros[] = {
-    {DEFAULT_SCHEMA, "times_two", {"x", nullptr}, {{nullptr, nullptr}}, R"( x * 2 )"},
-    {DEFAULT_SCHEMA, "default_to_times_two", {"x", nullptr}, {{"multiplier", "2"}, {nullptr, nullptr}}, R"( x * multiplier )"},
-    {nullptr, nullptr, {nullptr}, {{nullptr, nullptr}}, nullptr}
-};
-// clang-format on
-
-// To add a new SQL table macro, add a new macro to this array!
-// Copy and paste the top item in the array into the 
-// second-to-last position and make some modifications. 
-// (essentially, leave the last entry in the array as {nullptr, nullptr, {nullptr}, {{nullptr, nullptr}}, nullptr}
-
-// Keep the DEFAULT_SCHEMA (no change needed)
-// Replace "times_two_table" with a name for your macro
-// If your function has parameters without default values, add their names in quotes inside of the {}, with a nullptr at the end
-//      If you do not have parameters, simplify to {nullptr}
-// If your function has parameters with default values, add their names and values in double quotes inside of {}'s inside of the {}.
-//      If you have default values that are strings, wrap them in single quotes inside the double quotes 
-//          (Ex: {{"my_string":"'my_default_value'"}, {nullptr, nullptr}} )
-//      Be sure to keep {nullptr, nullptr} at the end
-//      If you do not have parameters with default values, simplify to {nullptr, nullptr}
-// Add the text of your SQL macro as a raw string with the format R"( select 42; )" 
-
-// clang-format off 
-static const DefaultTableMacro quack_table_macros[] = {
-    {DEFAULT_SCHEMA, "times_two_table", {"x", nullptr}, {{nullptr, nullptr}},  R"( SELECT x * 2 as output_column; )"},
-    {DEFAULT_SCHEMA, "default_to_times_two_table", {"x", nullptr}, {{"multiplier", "2"}, {nullptr, nullptr}},  R"( SELECT x * multiplier as output_column; )"},
-	{nullptr, nullptr, {nullptr}, {{nullptr, nullptr}}, nullptr}
-};
-// clang-format on
-
 inline void QuackScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
     auto &name_vector = args.data[0];
     UnaryExecutor::Execute<string_t, string_t>(
@@ -83,7 +35,31 @@ inline void QuackOpenSSLVersionScalarFun(DataChunk &args, ExpressionState &state
         });
 }
 
+// clang-format off
+// SQL Macros
+static DefaultMacro quack_macros[] = {
+    {DEFAULT_SCHEMA, "default_to_times_two", {"x", nullptr}, {{"multiplier", "2"}, {nullptr, nullptr}}, R"( x * multiplier )"},
+    {nullptr, nullptr, {nullptr}, {{nullptr, nullptr}}, nullptr}
+};
+
+// SQL Table Macros
+static const DefaultTableMacro quack_table_macros[] = {
+    {DEFAULT_SCHEMA, "default_to_times_two_table", {"x", nullptr}, {{"multiplier", "2"}, {nullptr, nullptr}},  R"( SELECT x * multiplier as output_column; )"},
+	{nullptr, nullptr, {nullptr}, {{nullptr, nullptr}}, nullptr}
+};
+// clang-format on
+
 static void LoadInternal(DatabaseInstance &instance) {
+    
+    // Register a scalar function
+    auto quack_scalar_function = ScalarFunction("quack", {LogicalType::VARCHAR}, LogicalType::VARCHAR, QuackScalarFun);
+    ExtensionUtil::RegisterFunction(instance, quack_scalar_function);
+
+    // Register another scalar function
+    auto quack_openssl_version_scalar_function = ScalarFunction("quack_openssl_version", {LogicalType::VARCHAR},
+                                                LogicalType::VARCHAR, QuackOpenSSLVersionScalarFun);
+    ExtensionUtil::RegisterFunction(instance, quack_openssl_version_scalar_function);
+
     // Register Scalar Macros
 	for (idx_t index = 0; quack_macros[index].name != nullptr; index++) {
 		auto info = DefaultFunctionGenerator::CreateInternalMacroInfo(quack_macros[index]);
@@ -94,15 +70,6 @@ static void LoadInternal(DatabaseInstance &instance) {
 		auto table_info = DefaultTableFunctionGenerator::CreateTableMacroInfo(quack_table_macros[index]);
         ExtensionUtil::RegisterFunction(instance, *table_info);
 	}
-    
-    // Register a scalar function
-    auto quack_scalar_function = ScalarFunction("quack", {LogicalType::VARCHAR}, LogicalType::VARCHAR, QuackScalarFun);
-    ExtensionUtil::RegisterFunction(instance, quack_scalar_function);
-
-    // Register another scalar function
-    auto quack_openssl_version_scalar_function = ScalarFunction("quack_openssl_version", {LogicalType::VARCHAR},
-                                                LogicalType::VARCHAR, QuackOpenSSLVersionScalarFun);
-    ExtensionUtil::RegisterFunction(instance, quack_openssl_version_scalar_function);
 }
 
 void QuackExtension::Load(DuckDB &db) {
